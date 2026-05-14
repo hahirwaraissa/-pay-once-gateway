cc182bde2df988f78c68187301daad782763575a
hahirwaraissa <hahirwa014@gmail.com>
2026-05-14 12:59:39 +0200

    Initial commit: Project setup with Spring Boot

---
+++ src/main/java/com/igirepay/pay_once_gateway/PayOnceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.igirepay.pay_once_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayOnceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayOnceGatewayApplication.class, args);
+	}
+
+}
